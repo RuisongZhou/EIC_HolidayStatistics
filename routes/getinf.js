d7358d94e6de9ec8ded2abbd9632083bfae61bc7
RuisongZhou <811437508@qq.com>
2018-03-21 00:00:57 +0800

    nothing

--- routes/getinf.js
+++ routes/getinf.js
@@ -1,8 +1,10 @@
+'use strict';
+
 let express = require('express');
 let router = express.Router();
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 router.post('/', urlencodedParser, function (req, res, next) {
-
+   
 });
